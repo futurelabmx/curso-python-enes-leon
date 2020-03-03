36da23478c20f2820421155eca291a042c319754
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-03-03 15:53:15 -0600

    Update II, 03/03.

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 
 eventos = [
 	{
-		"title": "Evento random #1"
+		"title": "Evento random #1",
+		"desc": "Descripción random sobre mi evento favorito #1."
 	},
 	{
-		"title": "Evento random #2"
+		"title": "Evento random #2",
+		"desc": "Descripción random sobre mi evento favorito #2."
+	},
+	{
+		"title": "Evento random #3",
+		"desc": "Descripción random sobre mi evento favorito #3."
 	}
 ]
 
@@ -17,6 +23,11 @@ eventos = [
 @app.route('/')
 def index():
 	return render_template('index.html', e=eventos)
+
+
+@app.route('/acerca-de')
+def about():
+	return render_template('about.html')
 
 
 if __name__ == "__main__":
