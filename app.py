3f6768a44ad96ab65ee8906ac80c5c22494b5a36
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-03-03 14:48:10 -0600

    Update I, 03/03.

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+eventos = [
+	{
+		"title": "Evento random #1"
+	},
+	{
+		"title": "Evento random #2"
+	}
+]
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', e=eventos)
 
 
 if __name__ == "__main__":
